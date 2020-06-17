14b90f29f10b267d588a47cce9fbfeaff586c4db
Aditya Soni <adityasoni5859@gmail.com>
2020-06-17 16:38:05 +0530

    written a program to test locks

---
+++ locks_test.py
@@ -0,0 +1,33 @@
+import time
+from multiprocessing import *
+
+
+def add_500_lock(total, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        total.value += 5
+        lock.release()
+
+
+def sub_500_lock(total, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        total.value -= 5
+        lock.release()
+
+
+if __name__ == '__main__':
+    total = Value('i', 500)
+    lock = Lock()
+    add_process = Process(target=add_500_lock, args=(total, lock))
+    sub_process = Process(target=sub_500_lock, args=(total, lock))
+
+    add_process.start()
+    sub_process.start()
+
+    add_process.join()
+    sub_process.join()
+
+    print(total.value)
