4ed40373460df2625596e38434978016069e9957
Aditya Soni <adityasoni5859@gmail.com>
2020-06-17 15:48:03 +0530

    extended the square program

--- square.py
+++ square.py
@@ -1,10 +1,27 @@
-def square(number):
-    result = number * number
-    print(f"The number {number} squared to {result}")
+import os
+from multiprocessing import *
+import time
+
+
+def square(numbers):
+    for number in numbers:
+        time.sleep(0.5)
+        result = number * number
+        print(
+            f"The number {number} squared to {result} for process name {current_process().name} and PID {os.getpid()}")
 
 
 if __name__ == '__main__':
-    numbers = [1, 2, 3, 4]
+    processes = []
+    numbers = range(100)
 
-    for number in numbers:
-        square(number)
+    for i in range(50):
+        process = Process(target=square, args=(numbers,))
+        processes.append(process)
+        # Processes are squared by creating a Process object and then calling its start() method
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    print("Multiprocessing complete")
